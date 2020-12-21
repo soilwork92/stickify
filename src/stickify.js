ec278b72ab13c1ee56812b4c19e8cd20f55d8758
Angel Petkov <angel.petkov92@gmail.com>
2020-12-21 23:03:51 +0200

    Updated core plugin file

--- src/stickify.js
+++ src/stickify.js
@@ -1,16 +1,21 @@
 /**
  * Sticky Header Plugin
  *
+ * Description: Simple plugin for fixing elements on scroll
+ *
+ * Author: Angel Petkov
+ * Author Website: https://quiztion.bg/
  *
  *
  * Options:
- *	- position (string)
- *  - animation (string)
- *  - direction (string)
- *  - reverse (boolean) (only for top and bottom)
+ *	- position (string): 'top', 'right', 'bottom', 'left'
+ *  - animation (string):
+ *  - animationDuration (numeric): in seconds - '0.4', '0.8', '1' and etc.
+ *  - reverse (boolean): true to hide element on scroll down and show in scroll up, false to fix the element
+ *  - width (string): in pixels or percentage, by default is 100%
+ *  - z-index (numeric): option to control the z-index, by default is 9999
  *
  */
-
 
 (function($) {
 
@@ -18,68 +23,82 @@
 
         var $selector = this;
 
+        // Set default options
         var defaultOptions = {
             position: 'top',
-            animation: 'fade',
-            direction: '',
-            reverse: true
+            animation: 'fade', //TODO: Add a few transition effects
+            animationDuration: '0.4',
+            reverse: false,
+            width: '100%',
+            zIndex: '9999'
         };
 
         var opts = $.extend({}, defaultOptions, options);
 
-        // Hide Header on on scroll down
-        var didScroll;
+        // Hide element on scroll down
+        var isScroll;
         var lastScrollTop = 0;
-        var delta = 5;
-        var navbarHeight = $selector.outerHeight();
+        var index = 5;
+        var elementHeight = $selector.outerHeight();
 
         $(window).scroll(function(event){
-            didScroll = true;
+            isScroll = true;
         });
 
         setInterval(function() {
-            if (didScroll) {
+            if (isScroll && opts.reverse === true) {
                 hasScrolled();
-                didScroll = false;
+                isScroll = false;
+            } else {
+                fixedHeader();
             }
         }, 250);
 
-        function hasScrolled() {
-            var st = $(this).scrollTop();
+        function fixedHeader() {
 
-            // Make sure they scroll more than delta
-            if(Math.abs(lastScrollTop - st) <= delta)
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 100) $selector
+                .css('position', 'fixed')
+                .css(opts.position, '0')
+                .css('width', opts.width)
+                .css('z-index', opts.zIndex)
+
+        }
+
+        function hasScrolled() {
+            var scrollTop = $(this).scrollTop();
+
+            // Make sure to scroll more than index
+            if(Math.abs(lastScrollTop - scrollTop) <= index)
                 return;
 
-            // If they scrolled down and are past the navbar, add class .nav-up.
-            // This is necessary so you never see what is "behind" the navbar.
-            if (st > lastScrollTop && st > navbarHeight){
+            if (scrollTop > lastScrollTop && scrollTop > elementHeight){
                 // Scroll Down
-                // $selector.removeClass('nav-down').addClass('nav-up');
                 $selector
                     .css('position', 'fixed')
-                    .css('top', '0')
-                    .css('transition', 'top 0.4s ease-in-out')
-                    .css('top', '-1000px')
-                    .css('width', '100%')
+                    .css(opts.position, '0')
+                    .css('transition', opts.position + ' ' + opts.animationDuration + 's ease-in-out')
+                    .css(opts.position, '-1000px')
+                    .css('width', opts.width)
+                    .css('z-index', opts.zIndex)
 
             } else {
                 // Scroll Up
-                if(st + $(window).height() < $(document).height()) {
-                    // $selector.removeClass('nav-up').addClass('nav-down');
+                if(scrollTop + $(window).height() < $(document).height()) {
                     $selector
-                        .css('top', '-1000px')
-                        .css('transition', 'top 0.4s ease-in-out')
-                        .css('top', '0')
-                        .css('width', '100%')
+                        .css(opts.position, '-1000px')
+                        .css('transition', opts.position + ' ' + opts.animationDuration + 's ease-in-out')
+                        .css(opts.position, '0')
+                        .css('width', opts.width)
+                        .css('z-index', opts.zIndex)
                 }
             }
 
-            lastScrollTop = st;
+            lastScrollTop = scrollTop;
         }
 
         return this;
     };
 
 })(jQuery);
-
